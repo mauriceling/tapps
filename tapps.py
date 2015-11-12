47644dccd5b53f05241a02a045a3f98487f0e29f
unknown <MauriceLing@BIOE-B224S4070.staff.main.ntu.edu.sg>
2015-11-12 09:25:06 +0800

    + added module documentation header

--- tapps.py
+++ tapps.py
@@ -1,3 +1,12 @@
+'''
+TAPPS: Technical (Analysis) and Applied Statistics.
+
+Date created: 11th November 2015
+'''
+import copads
+from copads.dataframe import Series
+from copads.dataframe import Dataframe
+
 import startup
 
 global session
