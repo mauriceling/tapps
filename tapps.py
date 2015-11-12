f0d29015aed41c3e3d73180db041397a3d3dd589
unknown <MauriceLing@BIOE-B224S4070.staff.main.ntu.edu.sg>
2015-11-12 11:36:22 +0800

    + linking command shell (not working yet)

--- tapps.py
+++ tapps.py
@@ -18,10 +18,26 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
+
+# attempt to import readline or pyreadline
+try:
+    import readline
+    readline_import = True
+    pyreadline_import = False
+except ImportError:
+    readline_import = False
+    try:
+        import pyreadline as readline
+        pyreadline_import = True
+    except ImportError:
+        readline_import = False
+        pyreadline_import = False
+        
 import copads
 from copads.dataframe import Series
 from copads.dataframe import Dataframe
 
+import commandshell
 import startup
 
 global session
@@ -33,3 +49,23 @@ def RunPlugin(plugin_name, parameters, session=session):
     plugin_name = 'plugin_' + plugin_name
     results = session[plugin_name]['main'](parameters)
     return results
+    
+    
+def RunShell():
+    shell = commandshell.Shell()
+    shell.header()
+    if readline_import:
+        shell.environment['readline_module'] = 'readline'
+    elif pyreadline_import:
+        shell.environment['readline_module'] = 'pyreadline'
+    else:
+        shell.environment['readline_module'] = None
+    if readline_import or pyreadline_import:
+        readline.set_completer(shell.completer)   # enables autocompletion
+        readline.parse_and_bind("tab: complete")
+    shell.cmdloop()
+    
+    
+if __name__ == '__main__':
+    RunShell()
+    sys.exit()
