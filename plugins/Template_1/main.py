51da98bd18e8c9ac48117038331e9b75b1c9d4ca
unknown <MauriceLing@BIOE-B224S4070.staff.main.ntu.edu.sg>
2015-11-13 14:28:50 +0800

    + added label summation operation to get the template to perform something

--- plugins/Template_1/main.py
+++ plugins/Template_1/main.py
@@ -12,4 +12,29 @@ def main(parameters):
     @return: session
     @rtype: dictionary
     '''
+    # Step 1: Pull out needed items / data from parameters dictionary
+    method = parameters['analytical_method']
+    dataframe = parameters['dataframe']
+    results = parameters['results']
+    # END Step 1: Pull out needed items / data from parameters dictionary
+    
+    # Step 2: Perform plugin operations
+    if method == 'label_summation':
+        (summation, labels) = label_summation(dataframe)
+        results.addData({'summation': summation}, 
+                        labels)
+    # END Step 2: Perform plugin operations
+    
+    # Step 3: Load dataframe and results back into parameters dictionary
+    parameters['dataframe'] = dataframe
+    parameters['results'] = results
+    # END Step 3: Load dataframe and results back into parameters dictionary
     return parameters
+    
+def label_summation(dataframe):
+    labels = dataframe.data.keys()
+    summation = [sum([float(item) 
+                      for item in dataframe.data[label]]) 
+                 for label in labels]
+    return (summation, labels)
+    
