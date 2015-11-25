9cdcbc57b2a906084f9c85056af99a5954c10766
Maurice Ling <mauriceling@acm.org>
2015-11-25 22:47:34 +0800

    # changing template name from title case to small case (2 of 2)

---
+++ plugins/template_1/main.py
@@ -0,0 +1,60 @@
+'''
+Template / Boiler plates for writing a TAPPS plugin.
+'''
+
+instructions = '''
+Provides user readable instructions on what the operations of the current 
+plugin, how to fill-in the parameters dictionary, and what options are 
+required/allowed by the plugin.
+'''
+
+parameters = \
+{'plugin_name': 'template',
+ 'analysis_name': None,
+ 'analytical_method': None,
+ 'dataframe': None,
+ 'results': None,
+}
+
+def main(parameters):
+    '''
+    Entry function for the plugin. This sample plugin will sum up the 
+    values in each data series within the data frame.
+    
+    @param parameters: set of parameters, including data frame, which are 
+    needed for the plugin to execute
+    @type parameters: dictionary
+    @return: parameters
+    @rtype: dictionary
+    '''
+    # Step 1: Pull out needed items / data from parameters dictionary
+    method = parameters['analytical_method']
+    dataframe = parameters['dataframe']
+    results = parameters['results']
+    # END Step 1: Pull out needed items / data from parameters dictionary
+    
+    # Step 2: Perform plugin operations
+    if method == 'summation':
+        (summation, labels) = series_summation(dataframe)
+        results.addData({'summation': summation}, 
+                        labels)
+    # END Step 2: Perform plugin operations
+    
+    # Step 3: Load dataframe and results back into parameters dictionary
+    parameters['dataframe'] = dataframe
+    parameters['results'] = results
+    # END Step 3: Load dataframe and results back into parameters dictionary
+    return parameters
+    
+def series_summation(dataframe):
+    '''
+    Function to perform summation of each series in the dataframe.
+    '''
+    labels = dataframe.data.keys()
+    series = dataframe.series_names
+    summation = []
+    for index in range(len(series)):
+        sdata = [float(dataframe.data[label][index]) for label in labels]
+        summation.append(sum(sdata))
+    return (summation, series)
+    
---
+++ plugins/template_1/manifest.py
@@ -0,0 +1,45 @@
+'''
+This file contains the properties / information about the plugin and should 
+be filled up by the plugin / app developer. This file is mandatory for each 
+plugin as it contains information to be used by Plugin / App Manager for 
+categorization and display to the user.
+
+All labels are required to be present; only those mandatory labels needs 
+to be filled up.
+'''
+
+# Name of the plugin / app (mandatory)
+name = 'template'
+
+# Version or release number of the plugin / app (mandatory)
+release = 1
+
+# Category which this plugin / app should belong to (mandatory)
+# Allowed categories are:
+# 1. statistics
+# 2. statistics.hypothesis
+# 3. statistics.model
+# 4. statistics.timeseries
+# - unclassified
+category = 'unclassified'
+
+# A short description of the plugin / app (not mandatory)
+shortDescription = '''
+'''
+
+# Long description of the plugin / app (not mandatory)
+longDescription = '''
+'''
+
+# URL of this project, if any (not mandatory)
+projectURL = ''
+
+# Person(s) to contact for any help or information regarding this plugin / app
+# (not mandatory)
+contactDetails = ''
+
+# License for this plugin / app (not mandatory)
+# If no license is given, it is deemed to be released into public domain 
+# for all uses, both academic and industry. 
+license = '''
+'''
