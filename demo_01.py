32b6bbd2e15c19571542ae50b6f2f6f59b71afcd
Maurice Ling <mauriceling@acm.org>
2015-11-28 01:14:13 +0800

    # combined into demo_01.py

--- demo_01.py
+++ demo_01.py
@@ -1,35 +1,44 @@
-import os
-import sys
-from pprint import pprint
+# import os
+# import sys
+# from pprint import pprint
+# 
+# from tapps import *
+# 
+# session = LoadCSV(session, os.sep.join([session['cwd'], 'data', 
+                    # 'STI_20151111_19871228.csv']))
+# session = AttachNewDataFrame(session, 'STI')
+# 
+# parameters = NewPluginParameters(session, 'template')
+# parameters['analysis_name'] = 'test'
+# parameters['plugin_name'] = 'template'
+# parameters['dataframe'] = session['MDF'].frames['STI']
+# parameters['analytical_method'] = 'summation'
+# session['parameters']['testingA'] = parameters
+# 
+# RunPlugin(session, 'testingA')
+# 
+# pprint(session)
+# pprint(parameters)
 
-from tapps import *
+show plugin list
+show plugin template
+show session
+show environment
 
-session = LoadCSV(session, 
-                  os.sep.join([session['cwd'], 'data', 
-                               'STI_20151111_19871228.csv']))
-session = AttachNewDataFrame(session, 'STI')
+set cwd /Users/mauriceling/Dropbox/MyProjects/tapps/data
+set separator ,
+set fillin None
+show environment
 
-parameters = NewPluginParameters(session, 'template')
+load csv STI_20151111_19871228.csv as STI
 
-parameters['analysis_name'] = 'test'
-parameters['plugin_name'] = 'template'
-parameters['dataframe'] = session['MDF'].frames['STI']
-parameters['analytical_method'] = 'summation'
+new template parameters as testingA
+set parameter analysis_name in testingA as test
+set parameter analytical_method in testingA as summation
+set parameter dataframe in testingA as STI
 
-session['parameters']['testingA'] = parameters
+runplugin testingA
 
-RunPlugin(session, 'testingA')
+new STI_summation dataframe from testingA results
 
-pprint(session)
-
-pprint(parameters)
-
-'''
-SET CWD /Users/mauriceling/Dropbox/MyProjects/tapps/data
-SET SEPARATOR ,
-SET FILLIN None
-
-LOAD CSV STI_20151111_19871228.csv as STI
-
-NEW template PARAMETERS AS testingA
-'''
+pythonshell
